1abf0cee0ef6d1a4a351e7cb60b78a5bcd552f18
Sagar Bansal <sagarbansal099@gmail.com>
2019-02-03 15:20:02 +0000

    alt3

--- index.php
+++ index.php
@@ -405,6 +405,14 @@
 
         </script>
 
+        <!-- START: Mobile Autoplay Video -->
+        <script>
+        var mobilevideo = document.getElementsByTagName("video")[0];
+        mobilevideo.setAttribute("playsinline", "");
+        mobilevideo.setAttribute("muted", "");
+        </script>
+        <!-- END: Mobile Autoplay Video -->
+
 
         <!--========== END FOOTER ==========-->
 
