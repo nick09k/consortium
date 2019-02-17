63b579a8d8bc6d9dcf0b08d850990a9e5a138c0e
root <anillakshya6@gmail.com>
2019-02-17 23:07:11 +0530

    kaam baki hai

--- dashboard.php
+++ dashboard.php
@@ -18,36 +18,62 @@
     $email = $_SESSION['email'];
 
     if(isset($_POST['swanewmem'])){
-      $name2 = $con->real_escape_string($_POST['name']);
-      $email2 = $con->real_escape_string($_POST['email']);
-      $contact2 = $con->real_escape_string($_POST['contact']);
+      $teamname = $con->real_escape_string($_POST['teamname']);
+      $number = $con->real_escape_string($_POST['number']);
+      $membername1 = $con->real_escape_string($_POST['membername1']);
+      $membername2 = $con->real_escape_string($_POST['membername2']);
+      $membername3 = $con->real_escape_string($_POST['membername3']);
+      $memberemail1 = $con->real_escape_string($_POST['memberemail1']);
+      $memberemail2 = $con->real_escape_string($_POST['memberemail2']);
+      $memberemail3 = $con->real_escape_string($_POST['memberemail3']);
+      $memberphone1 = $con->real_escape_string($_POST['memberphone1']);
+      $memberphone2 = $con->real_escape_string($_POST['memberphone2']);
+      $memberphone3 = $con->real_escape_string($_POST['memberphone3']);
 
-      if($name2 == "" || $email2 == "" || $contact2 == "" ){
+      // $field = array($membername1,$membername2,$membername3,$memberemail1,$memberemail2,$memberemail3,$memberphone1,$memberphone2,$memberphone3);
+
+      if($number == "" || $teamname == "" ){
         $msg = "Please fill all the details";
       }else{
-        $query = "SELECT * from Registrations where Email='$email'";
-        $result = mysqli_query($con,$query);
-        $num = mysqli_num_rows($result);
-
-        if($num == 0){
-          $query = "INSERT INTO Swadesh(Name,Main_Email,Email,Contact) VALUES('$name2','$email','$email2','$contact2')";
-          if(mysqlii_query($con,$query)){
-            $msg = "User added successfully";
-          }else{
-            echo("Error description: " . mysqli_error($con));
-          }
+        $tquery = "INSERT INTO Swadesh_team(Teamname) VALUES('$teamname') WHERE Email = '$email'";
+        if(mysqli_query($con,$tquery)){
+          echo "great";
         }else{
-          $query = "UPDATE Swadesh SET Main_Email='$email' WHERE Email = '$email2'";
-          if(mysqli_query($con,$query)){
-            $msg = "User added successfully";
+          echo("Error description: " . mysqli_error($con));
+        }
+        for($n = 1; $n <= $number; $n++){
+          $mn = 'membername'.$n;
+          $me = 'memberemail'.$n;
+          $mp = 'memberphone'.$n;
+
+          $mn = $$mn;
+          $me = $$me;
+          $mp = $$mp;
+          
+          $query = "SELECT * FROM Registrations WHERE Email='$me'";
+          $result = mysqli_query($con,$query);
+          $num = mysqli_num_rows($result);
+
+          if($num == 0){
+            $query = "INSERT INTO Swadesh(Name,Main_Email,Email,Contact) VALUES('$mn','$email','$me','$mp')";
+            if(mysqli_query($con,$query)){
+              $msg = "User added successfully";
+            }else{
+              echo("Error description: " . mysqli_error($con));
+            }
           }else{
-            echo("Error description: " . mysqli_error($con));
+            $data = mysqli_fetch_array($result);
+            $q = "UPDATE Registrations SET Swadesh = 1 WHERE Email = $memberemail";
+            if(mysqlii_query($con,$q)){
+              $msg = "User added successfully";
+            }else{
+              echo("Error description: " . mysqli_error($con));
+            }
           }
         }
       }
     }
-  }
-  else{
+  }else{
 
     $_SESSION['login_error'] = "Kindly Login First";
 
@@ -77,9 +103,10 @@
         <div class="g-bg-position--center js__parallax-window" style="background:#000;height:auto;">
             <div class="g-container--md g-text-center--xs g-padding-y-40--xs">
                 <h2 class="g-font-size-30--xs g-font-size-40--sm g-font-size-50--md g-margin-t-70--xs g-color--white g-letter-spacing--1">Dashboard</h2>
+                <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--red g-letter-spacing--2 g-margin-b-25--xs"><?php echo $msg; ?></p>
             </div>
         </div>
-        
+
         <!--========== END PROMO BLOCK ==========-->
 
         <!--========== PAGE CONTENT ==========-->
@@ -90,7 +117,7 @@
               <ul class="tabs tabs-fixed-width">
                 <li class="tab"><a class="active" href="#why" id="but_why">YOUR EVENTS</a></li>
                 <li class="tab"><a  href="#structure" id="but_structure">TICKETS</a></li>
-                
+
               </ul>
             </div>
             <div class="card-content code">
@@ -101,7 +128,7 @@
                 <div class="row product-grid">
                   <?php
                     $events = array('Swadesh','AdVenture','Pitch_Perfect','renderico','CEO','Teen_Titans','BizMantra','BizQuiz','ConsoWorld');
-                    $query = "SELECT * FROM Registrations WHERE Email='$email'";
+                    $query = "SELECT * FROM Registrations WHERE Email = '$email'";
                     $result = mysqli_query($con,$query);
                     $num = mysqli_num_rows($result);
                     if($num>0){
@@ -113,7 +140,7 @@
                         <div class="product-card__item-grid" style="background:url(img/events/<?php echo $events[$var] ?>.jpg)">
                             <div class="product-card__item-text">
                                 <h3 class="g-color--white"><?php echo $events[$var] ?></h3>
-                                
+
                                 <pclass="g-color--white"><i>#Event</i></p>
                             </div>
                         </div>
@@ -133,10 +160,10 @@
             <div id="structure" style="display:none;">
               <h5><b>Counter will be open soon</b></h5>
               <br/>
-              
+
 
               </div>
-              
+
 
             </div>
       </div>
@@ -152,17 +179,17 @@
           <?php
 
 
-            $query = "SELECT * FROM Swadesh_team WHERE Email='$email'";
+            $query = "SELECT * FROM Swadesh WHERE Main_Email='$email'";
             $result = mysqli_query($con,$query);
             $num = mysqli_num_rows($result);
             $data = $result->fetch_array(MYSQLI_ASSOC);
 
-            if($num!=0){
+            if($num > 4){
 
               echo '<h2 class="g-font-size-30--xs g-text-center--xs g-margin-t-70--xs g-color--white g-letter-spacing--1">'.$data['Name'].'</h2>';
 
-              $query = "SELECT * FROM Swadesh WHERE TeamID='$email'";
-            
+              $query = "SELECT * FROM Swadesh WHERE Main_Email='$email'";
+
               $num = mysqli_num_rows($result);
 
               echo "<ol>";
@@ -175,24 +202,33 @@
 
             else{
             ?>
-            
+
             <form class="center-block g-width-600--sm" method="post" action="">
                 <div class="permanent row">
+
                     <div class="col-sm-6 g-margin-b-30--xs">
-                          <input type="text" class="form-control s-form-v3__input" placeholder="* Team Name" name="name" style="text-transform: none" id="teamname">
+                          <input type="text" class="form-control s-form-v3__input" placeholder="* Team Name" name="teamname" style="text-transform: none" id="teamname">
                     </div>
+
 
                     <div class="col-sm-6 g-margin-b-30--xs">
                         <select type="number" pattern="[0-9]{11}" class="form-control s-form-v3__input" name="number" placeholder="* No. of members" id="members">
-                            <option value="" selected="" disabled="" hidden="">Number of members</option>
+                            <option value="" selected="" disabled="" hidden="">Add more members</option>
+                            <?php if($num == 3){ ?>
+                            <option value="1" style="color:black">1</option>
+                            <?php } ?>
+                            <?php if($num == 2){ ?>
+                            <option value="1" style="color:black">1</option>
+                            <option value="2" style="color:black">2</option>
+                            <?php } ?>
+                            <?php if($num == 1){ ?>
+                            <option value="1" style="color:black">1</option>
                             <option value="2" style="color:black">2</option>
                             <option value="3" style="color:black">3</option>
-                            <option value="4" style="color:black">4</option>
-                            <option value="5" style="color:black">5</option>
+                            <?php } ?>
                         </select>
                     </div>
 
-                    
                 </div>
                 <div class="g-text-center--xs">
                     <button type="submit" name="swanewmem" class="text-uppercase s-btn s-btn--md s-btn--white-brd g-radius--50 g-padding-x-70--xs g-margin-b-20--xs">Create Team</button>
@@ -201,7 +237,7 @@
           <?php
             }
           ?>
-          
+
         </div>
 
 
@@ -253,7 +289,7 @@
         <!--========== END FOOTER ==========-->
         <?php include("includes/script.php");?>
 
-        
+
         <script>
 
             var wid = $(".product-card__item-grid").width();
@@ -277,4 +313,3 @@
     </body>
     <!-- End Body -->
 </html>
-
--- register.php
+++ register.php
@@ -1,59 +1,62 @@
 <?php
-session_start();
-$pagetitle = 'Register | Consortium';
-$email = $_SESSION['email'];
-$name = $_SESSION['name'];
-$contact = $_SESSION['contact'];
-  $db_host = "localhost:3306";
-  $db_username = "conso19";
-  $db_pass = "Conso@123";
-  $db_name = "conso19";
+  @session_start();
+  $pagetitle = "Register | Consortium'19";
 
-  $con = mysqli_connect("$db_host","$db_username","$db_pass") or die ("could not connect to mysql");
-  mysqli_select_db($con,$db_name) or die ("no database");
+    $db_host = "localhost:3306";
+    $db_username = "conso19";
+    $db_pass = "Conso@123";
+    $db_name = "conso19";
 
-  $regquery = "CREATE TABLE IF NOT EXISTS Registrations(
-              ID INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-              Name VARCHAR(255) NOT NULL,
-              Email VARCHAR(255) NOT NULL,
-              Contact VARCHAR(255) NOT NULL,
-              Password VARCHAR(255) NOT NULL,
-              Swadesh TINYINT(1) DEFAULT '0',
-              AdVenture TINYINT(1) DEFAULT '0',
-              Pitch_Perfect TINYINT(1) DEFAULT '0',
-              renderico TINYINT(1) DEFAULT '0',
-              CEO TINYINT(1) DEFAULT '0',
-              Teen_Titans TINYINT(1) DEFAULT '0',
-              BizMantra TINYINT(1) DEFAULT '0',
-              BizQuiz TINYINT(1) DEFAULT '0',
-              ConsoWorld TINYINT(1) DEFAULT '0')
-              ";
+    $con = mysqli_connect("$db_host","$db_username","$db_pass") or die ("could not connect to mysql");
+    mysqli_select_db($con,$db_name) or die ("no database");
 
-  mysqli_query($con,$regquery);
+    $regquery = "CREATE TABLE IF NOT EXISTS Registrations(
+                ID INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                Name VARCHAR(255) NOT NULL,
+                Email VARCHAR(255) NOT NULL,
+                Contact VARCHAR(255) NOT NULL,
+                Password VARCHAR(255) NOT NULL,
+                Swadesh TINYINT(1) DEFAULT '0',
+                AdVenture TINYINT(1) DEFAULT '0',
+                Pitch_Perfect TINYINT(1) DEFAULT '0',
+                renderico TINYINT(1) DEFAULT '0',
+                CEO TINYINT(1) DEFAULT '0',
+                Teen_Titans TINYINT(1) DEFAULT '0',
+                BizMantra TINYINT(1) DEFAULT '0',
+                BizQuiz TINYINT(1) DEFAULT '0',
+                ConsoWorld TINYINT(1) DEFAULT '0',
+                otp VARCHAR(255) NOT NULL
+                )";
 
-  $eve = array('Swadesh','AdVenture','Pitch_Perfect','renderico','CEO','Teen_Titans','BizMantra','BizQuiz','ConsoWorld');
-  for($var = 0; $var < 9; $var++){
-    $evequery = "CREATE TABLE IF NOT EXISTS $eve[$var](
-              ID INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-              Name VARCHAR(255) NOT NULL,
-              Main_Email VARCHAR(255) NOT NULL,
-              Email VARCHAR(255) NOT NULL,
-              Contact VARCHAR(255) NOT NULL,
-              )";
+    mysqli_query($con,$regquery);
+
+    $eve = array('Swadesh','AdVenture','Pitch_Perfect','renderico','CEO','Teen_Titans','BizMantra','BizQuiz','ConsoWorld');
+    for($var = 0; $var < 9; $var++){
+      $evequery = "CREATE TABLE IF NOT EXISTS $eve[$var](
+                ID INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                Name VARCHAR(255) NOT NULL,
+                Main_Email VARCHAR(255) NOT NULL,
+                Email VARCHAR(255) NOT NULL,
+                Contact VARCHAR(255) NOT NULL
+                )";
     mysqli_query($con,$evequery);
-  }
+    }
 
-  $eve = array('Swadesh_team','AdVenture_team','Pitch_Perfect_team','renderico_team','BizMantra_team','BizQuiz_team','ConsoWorld_team');
-  for($var = 0; $var < 9; $var++){
-    $evequery = "CREATE TABLE IF NOT EXISTS $eve[$var](
-              ID INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-              Name VARCHAR(255) NOT NULL,
-              Email VARCHAR(255) NOT NULL,
-              Contact VARCHAR(255) NOT NULL,
-              )";
-    mysqli_query($con,$evequery);
-  }
+    $eve = array('Swadesh_team','AdVenture_team','Pitch_Perfect_team','renderico_team','BizMantra_team','BizQuiz_team','ConsoWorld_team');
+    for($var = 0; $var < 9; $var++){
+      $evequery = "CREATE TABLE IF NOT EXISTS $eve[$var](
+                ID INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                Teamame VARCHAR(255) NOT NULL,
+                Email VARCHAR(255) NOT NULL
+                )";
+      mysqli_query($con,$evequery);
+    }
 
+
+  if(isset($_SESSION['email'])){
+    $email = $_SESSION['email'];
+    $name = $_SESSION['name'];
+    $contact = $_SESSION['contact'];
   if(isset($_POST['sub_event'])) {
     $event = $con->real_escape_string($_POST['event']);
 
@@ -67,16 +70,25 @@ $contact = $_SESSION['contact'];
       if($num > 0){
         $data = mysqli_fetch_array($result);
         if($data[$event] != 1){
-          $q1 = "UPDATE Registrations SET $event = 1";
+          $q1 = "UPDATE Registrations SET $event = 1 WHERE Email = '$email'";
           mysqli_query($con,$q1);
 
           $q2 = "INSERT INTO $event(Name,Main_Email,Email,Contact) VALUES('$name','$email','$email','$contact')";
           mysqli_query($con,$q2);
+          $table = $event.$team . '_team';
+          $q3 = "INSERT INTO $table(Email) VALUES('$email')";
+          if(mysqli_query($con,$q3)){
+            echo "great";
+          }else{
+            echo("Error description: " . mysqli_error($con));
+          }
 
           $msg = "You have successfully registered for the event. View your <a href='dashboard.php'>Dashboard</a>.";
         }else{
           $msg = "You have already registered for the event! Visit your <a href='dashboard.php'>Dashboard</a>.";
         }
+      }else{
+        echo("Error description: " . mysqli_error($con));
       }
     }
   }
@@ -87,7 +99,6 @@ $contact = $_SESSION['contact'];
   <?php include("includes/head.php"); ?>
   <body class="back">
     <?php include("includes/header.php"); ?>
-    <?php if($_SESSION['email']){ ?>
     <div id="register">
         <div class="g-container--sm g-padding-y-80--xs g-padding-y-125--xsm">
             <div class="g-text-center--xs g-margin-b-60--xs">
@@ -118,27 +129,28 @@ $contact = $_SESSION['contact'];
             </form>
         </div>
     </div>
-    <?php }else{ ?>
-      <div id="register">
+      <!-- <div id="register">
           <div class="g-container--sm g-padding-y-80--xs g-padding-y-125--xsm">
               <div class="g-text-center--xs g-margin-b-60--xs">
                   <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--white-opacity g-letter-spacing--2 g-margin-b-25--xs">Register for an event</p>
-                  <h2 class="g-font-size-32--xs g-font-size-36--md g-color--white">Register Now</h2>
+                  <h2 class="g-font-size-32--xs g-font-size-36--md g-color--white">Register Now</h2> -->
                   <!-- <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--red g-letter-spacing--2 g-margin-b-25--xs">Login to register for an event</p> -->
                   <!-- <p id="message" class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--red g-letter-spacing--2 g-margin-b-25--xs"></p> -->
-              </div>
+              <!-- </div>
               <div class="g-text-center--xs">
                   <a href="regnew.php"><button name="events" class="text-uppercase s-btn s-btn--md s-btn--white-brd g-radius--50 g-padding-x-70--xs g-margin-b-20--xs">Register</button></a>
                   <a href="login.php"><button name="events" class="text-uppercase s-btn s-btn--md s-btn--white-brd g-radius--50 g-padding-x-70--xs g-margin-b-20--xs">Login</button></a>
               </div>
           </div>
-      </div>
-      <?php } ?>
+      </div> -->
 
       <?php include("includes/footer.php");?>
     </body>
   </html>
 
   <?php
-
+}else {
+  $_SESSION['login_error'] = "Kindly Login First";
+  header('location:login.php?ref=1');
+}
   ?>
