ea66fc1e9efe4e3990362ce6885c339bc4f7a1c0
sagban_ <sagarbansal099@gmail.com>
2019-03-05 21:21:10 +0530

    dd

--- wallstreet.php
+++ wallstreet.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" class="no-js">
-  <?php $pagetitle = 'Bizquiz | Consortium'; ?>
+  <?php $pagetitle = 'Wallstreet | Consortium'; ?>
 
   <!-- Begin Head -->
   <?php include("includes/head.php")?>
