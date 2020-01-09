2f10201ff69c2f4fed29f38657fa456b40959e30
root <anillakshya6@gmail.com>
2020-01-09 21:46:02 +0530

    fuck conso id

--- Swadesh.php
+++ Swadesh.php
@@ -95,7 +95,7 @@
     				<div id="structure" style="display:none;">
               <h5><b>How to participate</b></h5>
               <br/>
-              <p>To participate in Swades, visit our <a href="https://consortium.ecellvnit.org/">website</a> and register with your <b>consoID</b>. As soon your registration process gets complete you will get confirmation along with the questionnaire for the first round on your mail-id.  This would also unlock vast repository of resources to help you throughout the competition cycle and even beyond.</p>
+              <p>To participate in Swades, visit our <a href="https://consortium.ecellvnit.org/">website</a> and register with your <b>email id</b>. As soon your registration process gets complete you will get confirmation along with the questionnaire for the first round on your mail-id.  This would also unlock vast repository of resources to help you throughout the competition cycle and even beyond.</p>
               <br/>
               <h5><b>Structure</b></h5>
               <br/>
